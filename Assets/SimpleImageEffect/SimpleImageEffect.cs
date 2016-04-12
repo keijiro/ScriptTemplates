0b1050f8174403d9a986f253e3fc33396888cee2
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-04-12 18:41:18 +0900

    Initial commit

---
+++ Assets/SimpleImageEffect/SimpleImageEffect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Test
+{
+    [ExecuteInEditMode]
+    public class SimpleImageEffect : MonoBehaviour
+    {
+        #region Exposed properties
+
+        [SerializeField]
+        Shader _shader;
+
+        #endregion
+
+        #region Private objects
+
+        Material _material;
+
+        #endregion
+
+        #region MonoBehaviour functions
+
+        void OnEnable()
+        {
+            var shader = Shader.Find("Hidden/Test/SimpleImageEffect");
+            _material = new Material(shader);
+            _material.hideFlags = HideFlags.DontSave;
+        }
+
+        void OnDisable()
+        {
+            DestroyImmediate(_material);
+            _material = null;
+        }
+
+        void OnRenderImage(RenderTexture source, RenderTexture destination)
+        {
+            Graphics.Blit(source, destination, _material, 0);
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/SimpleImageEffect/SimpleImageEffect.shader
@@ -0,0 +1,34 @@
+﻿Shader "Hidden/Test/SimpleImageEffect"
+{
+    Properties
+    {
+        _MainTex("", 2D) = "white"{}
+    }
+
+    CGINCLUDE
+
+    #include "UnityCG.cginc"
+
+    sampler2D _MainTex;
+
+    fixed4 frag(v2f_img i) : SV_Target
+    {
+        fixed4 source = tex2D(_MainTex, i.uv);
+        return 1 - source;
+    }
+
+    ENDCG
+
+    SubShader
+    {
+        Cull Off ZWrite Off ZTest Always
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment frag
+            #pragma target 3.0
+            ENDCG
+        }
+    }
+}
